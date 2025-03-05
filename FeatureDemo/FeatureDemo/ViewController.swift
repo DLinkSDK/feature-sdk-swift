27326eb42c0318ae23d7ce0d4292756d2f7c7e64
frank <>
2025-03-05 09:52:17 +0800

    add the usage of collecting page appearance

--- FeatureDemo/FeatureDemo/ViewController.swift
+++ FeatureDemo/FeatureDemo/ViewController.swift
@@ -4,12 +4,19 @@
 //
 
 import UIKit
+import FeatureExtract
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        FeatureExtractLib.shared.onViewDidAppear(self)
     }
 }
 
